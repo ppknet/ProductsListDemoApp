0c5483d94067ba998543864bbb42e6666f2ce3d4
ppknet <praveen.kanase@gmail.com>
2020-05-19 13:45:20 +0800

    2 files modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {ProductListComponent } from './productlist.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,ProductListComponent ],
+  declarations: [ AppComponent, HelloComponent,ProductlistComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
